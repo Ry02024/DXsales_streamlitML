ea3874611aee8b50b44d4e605cddb58ec02fa8f1
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-11-07 08:40:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-import sys
-import os
 import streamlit as st
 import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -30,10 +28,10 @@ def main():
         test_file = st.sidebar.file_uploader("テストデータファイル (test.csv)", type=["csv"], key="test")
 
         if sales_file and item_file and category_file and test_file:
-            sales_df = pd.read_csv(sales_file)
-            item_df = pd.read_csv(item_file)
-            category_df = pd.read_csv(category_file)
-            test_df = pd.read_csv(test_file)
+            sales_df = load_data(sales_file, "売上データ")
+            item_df = load_data(item_file, "商品データ")
+            category_df = load_data(category_file, "カテゴリデータ")
+            test_df = load_data(test_file, "テストデータ")
 
             if st.button("前処理と特徴量生成を実行"):
                 with st.spinner("前処理と特徴量生成を実行しています..."):
@@ -45,14 +43,20 @@ def main():
         train_file = st.sidebar.file_uploader("訓練データファイル (train_df.csv)", type=["csv"], key="train")
         valid_file = st.sidebar.file_uploader("検証データファイル (validation_df.csv)", type=["csv"], key="valid")
 
+        # メイン画面で学習回数を指定
+        num_iterations = st.number_input("学習回数を指定してください", min_value=1, max_value=10000, value=1000, key="num_iterations")
+
         if train_file and valid_file:
-            train_df = pd.read_csv(train_file)
-            valid_df = pd.read_csv(valid_file)
+            train_df = load_data(train_file, "訓練データ")
+            valid_df = load_data(valid_file, "検証データ")
 
             if st.button("モデルのトレーニングを開始"):
-                with st.spinner("モデルのトレーニングを実行しています..."):
-                    execute_training(train_df, valid_df, model_save_dir)
-                st.success("モデルのトレーニングが完了しました。")
+                if num_iterations:
+                    with st.spinner("モデルのトレーニングを実行しています..."):
+                        execute_training(train_df, valid_df, model_save_dir, num_iterations)
+                    st.success("モデルのトレーニングが完了しました。")
+                else:
+                    st.error("学習回数を指定してください。")
 
     elif task_option == "予測":
         st.sidebar.title("予測用のデータファイルのアップロード")
@@ -60,7 +64,7 @@ def main():
         test_file = st.sidebar.file_uploader("テストデータファイル (test_df.csv)", type=["csv"], key="test")
 
         if model_file and test_file:
-            test_df = pd.read_csv(test_file)
+            test_df = load_data(test_file, "テストデータ")
 
             if st.button("予測を実行"):
                 with st.spinner("予測を実行しています..."):
